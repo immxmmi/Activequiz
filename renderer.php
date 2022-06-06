db1fbcd318002beadb41025320473486eb72a64a
Momo <immxmmi@gmx.at>
2022-06-06 18:53:32 +0200

    fix chart show

--- renderer.php
+++ renderer.php
@@ -550,7 +550,7 @@ class mod_activequiz_renderer extends plugin_renderer_base
         );
 
 
-        $inqcontrol .= html_writer::tag('button', '', array(
+        $inqcontrol .= html_writer::tag('button', 'Show Chart', array(
                 'class' => 'btn', 'id' => 'chart')
         );
 
